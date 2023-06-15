dafd42b41472851f871b3252906b0eaee9df2060
Anna <anna.brereton@gmail.com>
2023-06-15 15:04:34 +0100

    Added api url

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(BASE_URL);
+        const response = await fetch(BASE_URL + '/projects-db-api/projects');
         if (!response.ok) {
           throw new Error("Failed to fetch projects");
         }
